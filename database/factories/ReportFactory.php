e3b0ff1cdf8daca9fd0bbd5d2fe11f45641e2ba6
saber13812002 <saber.tabatabaee@gmail.com>
2020-06-17 00:59:06 +0430

    laravel7 component https://www.youtube.com/watch?v=cQWeTnOwlm4

---
+++ database/factories/ReportFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Report;
+use Faker\Generator as Faker;
+
+$factory->define(Report::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
