f882f96dc0565160a554bf9d4f1ae368fe82cc46
saber13812002 <saber.tabatabaee@gmail.com>
2020-07-26 01:27:17 +0430

    install on cpanel

--- install.bat
+++ install.bat
@@ -3,3 +3,5 @@ composer install
  cp /home2/pfajazii/trello0/public/index.php /home2/pfajazii/public_html
  cp /home2/pfajazii/trello0/.env /home2/pfajazii/trello
  cp /home2/pfajazii/trello0/public/.htaccess /home2/pfajazii/public_html/
+ chmod +x install.bat
+ ./install.bat
