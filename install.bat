79a33dbe6ae43a754c62e2de48620ca307e87687
saber13812002 <saber.tabatabaee@gmail.com>
2020-07-25 20:57:33 +0430

    Install on cpanel

---
+++ install.bat
@@ -0,0 +1,5 @@
+composer install
+ cp -R /home2/pfajazii/trello/public /home2/pfajazii/public_html
+ cp /home2/pfajazii/trello0/public/index.php /home2/pfajazii/public_html
+ cp /home2/pfajazii/trello0/.env /home2/pfajazii/trello
+ cp /home2/pfajazii/trello0/public/.htaccess /home2/pfajazii/public_html/
