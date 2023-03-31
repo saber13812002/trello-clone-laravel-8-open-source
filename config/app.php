34376d16efd6fe819adf074e47d8cb023ccd1d51
saber <saber.tabatabaee@gmail.com>
2023-03-31 14:06:50 +0330

    upgrade to php 8.2

--- config/app.php
+++ config/app.php
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Hekmatinasser\Verta\VertaServiceProvider::class,
+        Hekmatinasser\Verta\Laravel\VertaServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -227,7 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Verta' => Hekmatinasser\Verta\Verta::class,
+        'Verta' => Hekmatinasser\Verta\Laravel\VertaServiceProvider::class,
 
     ],
 
